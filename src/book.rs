16019f8e8d34c978336a856c7c216e31dda076c2
codeworm96 <codeworm96@outlook.com>
2018-03-28 00:20:48 +0800

    Add rocket

---
+++ src/book.rs
@@ -0,0 +1,11 @@
+pub struct Book {
+    pub stub: i64,
+}
+
+impl Book {
+    pub fn new() -> Book {
+        Book {
+            stub: 0,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+mod book;
+
+use std::sync::{Arc, Mutex};
+
+#[get("/gossip")]
+fn gossip(book: rocket::State<Arc<Mutex<book::Book>>>) -> String {
+    format!("gossip, gossip {}", book.lock().unwrap().stub)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let status = Arc::new(Mutex::new(book::Book::new()));
+    rocket::ignite()
+        .manage(status.clone())
+        .mount("/", routes![gossip]).launch();
 }
